043597e19fc14168f1298a3454f661d4afc66e80
khawla - niveau global <khawlabenalkacem@gmail.com>
2023-02-25 13:25:13 +0100

    create new project with Nestjs

--- src/Controllers/MathController.ts
+++
@@ -1,6 +0,0 @@
-import express, { Request, Response,NextFunction} from 'express'
-const app = express()
-import {ValidateParams} from '../Middlewares/validateParams;
-import{Handler }from '../Services/Mathservices';
-
-app.use("/calcul",ValidateParams,Handler);
